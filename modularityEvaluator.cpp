57520fe8a029149f1c0265aa4a8231cff9de113c
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2016-02-11 02:11:51 -0500

    Modularity computer finally works
    Maybe not exactly in the intended way, but still I managed to
    encapsulate the whole computation including the format conversion
    into one function.
    
    	modified:   modularityEvaluator.cpp

--- modularityEvaluator.cpp
+++ modularityEvaluator.cpp
@@ -9,51 +9,13 @@
 #define RANDOM_SEED 10000
 #endif // RANDOM_SEED
 
-char *filename = NULL;
-char *filename_w = NULL;
-char *filename_part = NULL;
-int type       = UNWEIGHTED;
-int nb_pass    = 0;
-double precision = 0.000001;
-int display_level = -2;
-int k1 = 16;
+#ifndef PRECISION
+#define PRECISION 1e-6
+#endif // PRECISION
 
-bool verbose = false;
-
-int main(int argc, char **argv)
-{
-	srand(RANDOM_SEED);
-	std::vector<int> topology = {4, -2, 2};
-
-	std::cout << "Testing non-recurrent network...\n";
-
-	std::string inputFile("tests/nonrecurrentGraph.bin");
-	std::string weightsFile("tests/nonrecurrentGraph.wgt");
-	Graph testGraph0(inputFile.c_str(), weightsFile.c_str(), WEIGHTED);
-	printGraph(testGraph0);
-	testGraph0.display();
-
-	Graph testGraph1("42 1 0 0 1 0 0.5 0.5 0 0 0 0 0 1 0.1 0 1", topology);
-	printGraph(testGraph1);
-	testGraph1.display();
-
-	std::cout << std::endl << "Testing network with recurrent connections...\n";
-
-	inputFile = "tests/recurrentGraph.bin";
-	weightsFile = "tests/recurrentGraph.wgt";
-//	Graph testGraph2(inputFile.c_str(), weightsFile.c_str(), UNWEIGHTED);
-	Graph testGraph2(inputFile.c_str(), weightsFile.c_str(), WEIGHTED);
-	printGraph(testGraph2);
-	testGraph2.display();
-
-	Graph testGraph3("41 1 0 0 1 0 0.5 0.5 0 0.2 0.5 0 -0.2 1 0.1 0 1", topology);
-	printGraph(testGraph3);
-	testGraph3.display();
-
-//	Community c(inputFile.c_str(), NULL, type, -1, precision);
-//	type = WEIGHTED;
-//	Community c(inputFile.c_str(), weightsFile.c_str(), type, -1, precision);
-/*	Graph g;
+double modularity(const std::string& annGenotype, const std::vector<int>& annTopology) {
+	Community c(annGenotype, annTopology, -1, PRECISION);
+	Graph g;
 	bool improvement = true;
 	double new_mod;
 
@@ -61,11 +23,24 @@ int main(int argc, char **argv)
 		improvement = c.one_level();
 		new_mod = c.modularity();
 		g = c.partition2graph_binary();
-		c = Community(g, -1, precision);
+		c = Community(g, -1, PRECISION);
 
 	} while(improvement);
 
-  std::cout << new_mod << endl;*/
+	return new_mod;
+}
+
+int main(int argc, char **argv) {
+	srand(RANDOM_SEED);
+	std::vector<int> annTopology = {4, -2, 2};
+	std::vector<std::string> annGenomes = {
+		"43 1 0 0 1 0 0.5 0.5 0 0 0 0 0 1 0.1 0 1",
+		"42 1 0 0 1 0 0.5 0.5 0 0 0 0 0 1 0.9 0 1",
+		"41 1 0 0 1 0 0.5 0.5 0 0.2 0.5 0.5 -0.2 1 0.1 0 1"
+	};
+
+	for(auto gen : annGenomes)
+	  std::cout << gen << " Q:" << modularity(gen, annTopology) << std::endl;
 
 	return 0;
 }
